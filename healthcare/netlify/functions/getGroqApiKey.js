b7633f932cf3f9fad3ddcd83d277d6b5e67a4959
PRAVEEN1670 <117348168+PRAVEEN1670@users.noreply.github.com>
2025-04-25 06:29:53 +0530

    Add files via upload

--- healthcare/netlify/functions/getGroqApiKey.js
+++ healthcare/netlify/functions/getGroqApiKey.js
@@ -1,1 +1,11 @@
-
+exports.handler = async () => {
+    const apiKey = process.env.GROQ_API_KEY;
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ apiKey: apiKey || "API Key Not Found" })
+    };
+};
